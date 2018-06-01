378204512daf39ab83aef78c66386ac19c310bb4
DESKTOP-1Q1MO05\erazor1980 <slevinkelevra1980@gmail.com>
2018-06-01 22:16:45 +0200

    - added writeup (readme.md) - some more parameter tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -7,7 +7,7 @@ using CppAD::AD;
 
 // TODO: Set the timestep length and duration
 size_t N = 10;
-double dt = 0.2;
+double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -23,7 +23,7 @@ const double Lf = 2.67;
 
 // Both the reference cross track and orientation errors are 0.
 // The reference velocity is set to 40 mph.
-double ref_v = 40;
+double ref_v = 60;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
@@ -56,13 +56,13 @@ class FG_eval {
 	{
       fg[0] += 10 * CppAD::pow(vars[cte_start + t], 2);
       fg[0] += 10 * CppAD::pow(vars[epsi_start + t], 2);
-      fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
+      fg[0] += 0.2 * CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
     for (int t = 0; t < ( int )N - 1; t++) 
 	{
-      fg[0] += 500 * CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 300 * CppAD::pow(vars[delta_start + t], 2);
       fg[0] += 5 * CppAD::pow(vars[a_start + t], 2);
     }
 
@@ -70,7 +70,7 @@ class FG_eval {
     for (int t = 0; t < ( int )N - 2; t++) 
 	{
       fg[0] += 10 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
-      fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+      fg[0] += 5 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 	
 	// Initial constraints
